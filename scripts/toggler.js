d89a117467607ffead46bdd074665432fceac687
kenwilcox <wilcoxk@gmail.com>
2014-11-16 16:55:39 -0700

    pass class to toggle

--- scripts/toggler.js
+++ scripts/toggler.js
@@ -1,7 +1,10 @@
 //"use strict";
 
-var toggleNextElement = function (event) {
-  console.log('Something will be toggled.');
-  var elementToToggle = event.target.nextElementSibling;
-  elementToToggle.classList.toggle('hidden');
+var toggleNextElement = function(className) {
+  var _toggleNextElement = function(event) {
+    
+    var elementToToggle = event.target.nextElementSibling;
+    elementToToggle.classList.toggle(className);
+  };
+  return _toggleNextElement;
 };
