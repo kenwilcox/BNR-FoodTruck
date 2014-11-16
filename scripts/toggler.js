a59c5a4ffb43ffa71cf699c26eed827090d43084
kenwilcox <wilcoxk@gmail.com>
2014-11-16 16:30:27 -0700

    toggle

--- scripts/toggler.js
+++ scripts/toggler.js
@@ -1,3 +1,5 @@
-var toggleNextElement = function () {
+var toggleNextElement = function (event) {
   console.log('Something will be toggled.');
+  var elementToToggle = event.target.nextElementSibling;
+  elementToToggle.classList.toggle('hidden');
 };
