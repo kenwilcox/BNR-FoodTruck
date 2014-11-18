e6c83aa1accba5fa9543df13423565ee2850b985
kenwilcox <wilcoxk@gmail.com>
2014-11-17 18:05:19 -0700

    messing with event.path and event.shiftKey

--- scripts/toggler.js
+++ scripts/toggler.js
@@ -1,10 +1,18 @@
 //"use strict";
 
-var toggleNextElement = function(className) {
+var toggleNextElement = function(className, newColor) {
   var _toggleNextElement = function(event) {
-    
-    var elementToToggle = event.target.nextElementSibling;
-    elementToToggle.classList.toggle(className);
+    if (!event.shiftKey) {
+      var elementToToggle = event.target.nextElementSibling;
+      elementToToggle.classList.toggle(className);
+    } else {
+      var items = [].slice.call(event.path);
+      items.forEach(function(item) {
+        if (item && item.style) {
+          item.classList.toggle(newColor);
+        }
+      });
+    }
   };
   return _toggleNextElement;
 };
